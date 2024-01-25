c5751ce1edddb067f896a3a93429aedcc585fee2
pore133 <94101325+pore133@users.noreply.github.com>
2024-01-25 12:38:15 +0900

    Add files via upload

---
+++ JNICDriver.c
@@ -0,0 +1,144 @@
+#include <fcntl.h>
+#include <jni.h>
+#include <string.h>
+#include <sys/ioctl.h>
+
+#define FAIL -1
+#define SUCCESS 1
+
+#define DO 0
+#define MI 4
+#define SO 7
+#define HIGH_DO 12
+#define CENTER 1
+
+#define UP_TEXT "Up"
+#define DOWN_TEXT "Down"
+#define LEFT_TEXT "Left"
+#define RIGHT_TEXT "Right"
+#define CENTER_TEXT "Center"
+
+#define LED_PATH "/dev/sm9s5422_led"
+#define VIBRATOR_PATH "/dev/sm9s5422_perivib"
+#define PIEZO_PATH "/dev/sm9s5422_piezo"
+#define SEGMENT_PATH "/dev/sm9s5422_segment"
+#define PUSHBUTTON_PATH "/dev/sm9s5422_interrupt"
+#define TEXTLCD_PATH "/dev/sm9s5422_textlcd"
+
+#define TEXTLCD_BASE 0x56
+#define TEXTLCD_FUNCTION_SET _IO(TEXTLCD_BASE, 0x31)
+
+#define TEXTLCD_DISPLAY_ON _IO(TEXTLCD_BASE, 0x32)
+#define TEXTLCD_DISPLAY_OFF _IO(TEXTLCD_BASE, 0x33)
+#define TEXTLCD_DISPLAY_CURSOR_ON _IO(TEXTLCD_BASE, 0x34)
+#define TEXTLCD_DISPLAY_CURSOR_OFF _IO(TEXTLCD_BASE, 0x35)
+
+#define TEXTLCD_CURSOR_SHIFT_RIGHT _IO(TEXTLCD_BASE, 0x36)
+#define TEXTLCD_CURSOR_SHIFT_LEFT _IO(TEXTLCD_BASE, 0x37)
+
+#define TEXTLCD_ENTRY_MODE_SET _IO(TEXTLCD_BASE, 0x38)
+#define TEXTLCD_RETURN_HOME _IO(TEXTLCD_BASE, 0x39)
+#define TEXTLCD_CLEAR _IO(TEXTLCD_BASE, 0x3a)
+
+#define TEXTLCD_DD_ADDRESS_1 _IO(TEXTLCD_BASE, 0x3b)
+#define TEXTLCD_DD_ADDRESS_2 _IO(TEXTLCD_BASE, 0x3c)
+#define TEXTLCD_WRITE_BYTE _IO(TEXTLCD_BASE, 0x3d)
+
+int led_fd = -1;
+int vibrator_fd = -1;
+int piezo_fd = -1;
+int segment_fd = -1;
+int pushbutton_fd = -1;
+int textlcd_fd = -1;
+
+JNIEXPORT jint JNICALL Java_com_wz_jnidriver_JNIDriver_openDrivers(JNIEnv *env, jclass class) {
+    led_fd = open(LED_PATH, O_WRONLY);
+    vibrator_fd = open(VIBRATOR_PATH, O_WRONLY);
+    piezo_fd = open(PIEZO_PATH, O_WRONLY);
+    segment_fd = open(SEGMENT_PATH, O_WRONLY);
+    pushbutton_fd = open(PUSHBUTTON_PATH, O_RDONLY);
+    textlcd_fd = open(TEXTLCD_PATH, O_WRONLY);
+
+    if (led_fd < 0 || vibrator_fd < 0 || piezo_fd < 0 || segment_fd < 0 || pushbutton_fd < 0 || textlcd_fd < 0) return FAIL;
+    return SUCCESS;
+}
+
+JNIEXPORT void JNICALL Java_com_wz_jnidriver_JNIDriver_closeDrivers(JNIEnv *env, jclass class) {
+    if (led_fd > 0) close(led_fd);
+    if (vibrator_fd > 0) close(vibrator_fd);
+    if (piezo_fd > 0) close(piezo_fd);
+    if (segment_fd > 0) close(segment_fd);
+    if (pushbutton_fd > 0) close(pushbutton_fd);
+    if (textlcd_fd > 0) close(textlcd_fd);
+}
+
+JNIEXPORT void JNICALL Java_com_wz_jnidriver_JNIDriver_writeLED(JNIEnv *env, jobject obj, jbyteArray arr, jint count) {
+    if (led_fd < 0) return;
+
+    jbyte *led_chars = (*env)->GetByteArrayElements(env, arr, 0);
+    write(led_fd, (unsigned char *)led_chars, count);
+    (*env)->ReleaseByteArrayElements(env, arr, led_chars, 0);
+}
+
+JNIEXPORT void JNICALL Java_com_wz_jnidriver_JNIDriver_writeVibrator(JNIEnv *env, jobject obj, jchar char_command) {
+    if (vibrator_fd < 0) return;
+
+    int command = (int)char_command;
+    write(vibrator_fd, &command, sizeof(command));
+}
+
+JNIEXPORT void JNICALL Java_com_wz_jnidriver_JNIDriver_writePiezo(JNIEnv *env, jobject obj, jchar char_note) {
+    if (piezo_fd < 0) return;
+
+    int note = (int)char_note;
+    write(piezo_fd, &note, sizeof(note));
+}
+
+JNIEXPORT void JNICALL Java_com_wz_jnidriver_JNIDriver_writeSegment(JNIEnv *env, jobject obj, jbyteArray arr, jint duration) {
+    if (segment_fd < 0) return;
+
+    jbyte *segment_chars = (*env)->GetByteArrayElements(env, arr, 0);
+    write(segment_fd, (unsigned char *)segment_chars, duration);
+    (*env)->ReleaseByteArrayElements(env, arr, segment_chars, 0);
+}
+
+JNIEXPORT jint JNICALL Java_com_wz_jnidriver_JNIDriver_getInterrupt(JNIEnv *env, jobject obj) {
+    char read_data[100];
+    int read_flag = read(pushbutton_fd, &read_data, 100);
+
+    if (read_flag < 0) return -1;
+
+    if (strcmp(read_data, CENTER_TEXT) == 0) return CENTER;
+    if (strcmp(read_data, DOWN_TEXT) == 0) return DO;
+    if (strcmp(read_data, LEFT_TEXT) == 0) return MI;
+    if (strcmp(read_data, RIGHT_TEXT) == 0) return SO;
+    if (strcmp(read_data, UP_TEXT) == 0) return HIGH_DO;
+
+    return -1;
+}
+
+JNIEXPORT void JNICALL Java_com_wz_jnidriver_JNIDriver_writeLCDLine(JNIEnv *env, jclass class, jstring text, jint len, jint slot) {
+    if (textlcd_fd < 0) return;
+
+    jboolean iscopy;
+    int i = 0;
+    const char *text_utf = (*env)->GetStringUTFChars(env, text, &iscopy);
+
+    if (slot == 0) {
+        ioctl(textlcd_fd, TEXTLCD_DD_ADDRESS_1, NULL);
+    } else {
+        ioctl(textlcd_fd, TEXTLCD_DD_ADDRESS_2, NULL);
+    }
+
+    for (i = 0; i < len; i++) {
+        ioctl(textlcd_fd, TEXTLCD_WRITE_BYTE, text_utf[i]);
+    }
+
+    (*env)->ReleaseStringUTFChars(env, text, text_utf);
+}
+
+JNIEXPORT void JNICALL Java_com_wz_jnidriver_JNIDriver_clearLCD(JNIEnv *env, jclass class) {
+    if (textlcd_fd < 0) return;
+
+    ioctl(textlcd_fd, TEXTLCD_CLEAR, NULL);
+}
---
+++ MainActivity.java
@@ -0,0 +1,459 @@
+package com.wz.melodymemorize;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.wz.jnidriver.JNIDriver;
+import com.wz.jnidriver.JNIListener;
+
+public class MainActivity extends Activity implements JNIListener {
+    static JNIDriver driver = new JNIDriver();
+    static byte[] ledData = {0, 0, 0, 0, 0, 0, 0, 0};
+    static byte[] notes = new byte[500];
+    
+    
+    static ImageView arrowBoard;
+    static TextView gameMessage;
+    
+    static final int NOT_STARTED = 0;
+    static final int LOCKED = 1;
+    static final int OPEN = 2;
+    
+    static final int DO = 0;
+    static final int MI = 4;
+    static final int SO = 7;
+    static final int HIGH_DO = 12;
+    static final int CENTER = 1;
+    
+    static final String WAITING_MESSAGE = "【CENTER】 버튼을 눌러 게임을 시작해 주세요.";
+    static final String START_MESSAGE = "게임을 시작합니다!";
+    static final String INTRODUCE_MESSAGE_1 = "【HOW TO PLAY】\n1. 멜로디가 연주되면 주의 깊게 들어 주세요.";
+    static final String INTRODUCE_MESSAGE_2 = "【HOW TO PLAY】\n2. 멜로디가 끝나면 연주된 멜로디를 방향키를 눌러 똑같이 연주해 주세요.";
+    static final String INTRODUCE_MESSAGE_3 = "【HOW TO PLAY】\n3. 멜로디를 잘못 연주해 LIFE가 모두 소진되면, 게임은 끝납니다!";
+    static final String INTRODUCE_MESSAGE_4 = "이제 게임을 시작하겠습니다!";
+    static final String LISTEN_MESSAGE = "멜로디가 연주되고 있습니다! 주의 깊게 들어 주세요.";
+    static final String PLAY_MESSAGE = "이제, 들려드린 멜로디를 똑같이 연주해 주세요!";
+    static final String CORRECT_MESSAGE = "맞았습니다! 다음 라운드로 이동합니다.";
+    static final String ERROR_MESSAGE = "게임에 필요한 드라이버를 불러오는 데 실패하였습니다. 앱을 재시작해 주세요.";
+    static final String TRYAGAIN_MESSAGE = "다시 멜로디를 들려드리겠습니다.";
+    static final byte[] EMPTY_BIT = {0, 0, 0, 0, 0, 0, 0, 0};
+    
+    static ScoreThread scoreThread;
+    static boolean isScoreThreadRunning = false;
+    static boolean isAnnounceThreadRunning = false;
+    static boolean firstRun = true;
+    static int buttonLocker = NOT_STARTED;
+    static int score;
+    static int life;
+    static int round;
+    static int guessIndex;
+    static int listenIndex;
+    static Handler delayHandler = new Handler();
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arrowBoard = (ImageView) findViewById(R.id.arrowBoard);
+        gameMessage = (TextView) findViewById(R.id.gameMessage);
+		scoreThread = new ScoreThread();
+        scoreThread.start();
+        driver.setListener(this);
+        
+        waitingPhase();
+    }
+    
+    public static void waitingPhase() {
+        buttonLocker = NOT_STARTED;
+        isScoreThreadRunning = false;
+        driver.clearLCDText();
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		driver.setLCDText("PRESS CENTER", 0);
+                driver.setLCDText("TO START", 1);
+        	}
+        }, 100);
+        
+        gameMessage.setText(WAITING_MESSAGE);
+    }
+
+	public static void startPhase() {
+        score = 0;
+        life = 3;
+        round = 0;
+        buttonLocker = LOCKED;
+        
+        isScoreThreadRunning = true;
+        driver.clearLCDText();
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		driver.setLCDText("GAME START", 0);
+        	}
+        }, 100);
+        
+        gameMessage.setText(START_MESSAGE);
+        
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		if (firstRun) {
+                    firstRun = false;
+                    introducePhase();
+                } else {
+                	roundAnnouncePhase();
+                }
+        	}
+        }, 3000);
+    }
+    
+    public static void introducePhase() {
+    	delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		gameMessage.setText(INTRODUCE_MESSAGE_1);
+        	}
+        }, 2500);
+    	
+    	delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		gameMessage.setText(INTRODUCE_MESSAGE_2);
+        	}
+        }, 5000);
+    	
+    	delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		gameMessage.setText(INTRODUCE_MESSAGE_3);
+        	}
+        }, 7500);
+    	
+    	delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		gameMessage.setText(INTRODUCE_MESSAGE_4);
+        	}
+        }, 10000);
+    	
+    	delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		isScoreThreadRunning = true;
+        		roundAnnouncePhase();
+        	}
+        }, 15000);
+    }
+    
+    public static void roundAnnouncePhase() {
+        round += 1;
+        
+        driver.clearLCDText();
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		driver.setLCDText("ROUND " + round, 0);
+                driver.setLCDText("LIFE: " + life, 1);
+        	}
+        }, 100);
+        
+        gameMessage.setText(round + " 단계");
+        
+        createRandomNote(round);
+        
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		listenPhase();
+        	}
+        }, 3000);
+    }
+    
+    public static void listenPhase() {
+    	listenIndex = 1;
+        driver.clearLCDText();
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		driver.setLCDText("ROUND " + round, 0);
+                driver.setLCDText("LIFE: " + life, 1);
+        	}
+        }, 100);
+        
+        gameMessage.setText(LISTEN_MESSAGE);
+        
+        listenSingleNote();
+    }
+    
+    public static void playPhase() {
+        gameMessage.setText(PLAY_MESSAGE);
+        
+        guessIndex = 1;
+        buttonLocker = OPEN;
+    }
+    
+    public static void correctAnswerPhase() {
+        buttonLocker = LOCKED;
+        
+        driver.clearLCDText();
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		driver.setLCDText("GREAT JOB", 0);
+                driver.setLCDText("LIFE: " + life, 1);
+        	}
+        }, 100);
+        
+        gameMessage.setText(CORRECT_MESSAGE);
+        
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		roundAnnouncePhase();
+        	}
+        }, 3000);
+    }
+    
+    public static void wrongAnswerPhase() {
+        buttonLocker = LOCKED;
+        life -= 1;
+        
+        driver.clearLCDText();
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		driver.setLCDText("WRONG MELODY", 0);
+                driver.setLCDText("LIFE: " + life, 1);
+                
+        	}
+        }, 100);
+        
+        gameMessage.setText("틀렸습니다! " + life + " 번의 기회가 남았습니다.");
+        driver.setVibrator(1);
+        
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		driver.setVibrator(0);
+        	}
+        }, 1000);
+        
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		if (life <= 0) {
+                    gameOverPhase();
+                } else {
+                    tryAgainPhase();
+                }
+        	}
+        }, 3000);
+    }
+    
+    public static void tryAgainPhase() {
+    	driver.clearLCDText();
+    	delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		driver.setLCDText("LETS TRY AGAIN", 0);
+                driver.setLCDText("LIFE: " + life, 1);
+        	}
+        }, 100);
+        
+        gameMessage.setText("다시 멜로디를 들려드리겠습니다.");
+            
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		listenPhase();
+        	}
+        }, 3000);
+    }
+    
+    public static void gameOverPhase() {
+    	driver.clearLCDText();
+    	delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		driver.setLCDText("GAME OVER", 0);
+                driver.setLCDText("NICE PLAY", 1);
+        	}
+        }, 100);
+        
+        gameMessage.setText("【GAME OVER】\n" + round + " 라운드에서 탈락하셨으며,\n" + score + " 점을 기록하셨습니다.");
+            
+        delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		waitingPhase();
+        	}
+        }, 5000);
+    }
+    
+    @Override
+    public void onPause() {
+        driver.close();
+        super.onPause();
+    }
+    
+    @Override
+    protected void onResume() {
+    	if (driver.open() < 0) {
+            gameMessage.setText(ERROR_MESSAGE);
+    	}
+        super.onResume();
+    }
+    
+    public static void delay(int miliseconds) {
+        try {
+            Thread.sleep(miliseconds);
+        } catch (InterruptedException e) {
+        	e.printStackTrace();
+        }
+    }
+    
+    
+    public static void createRandomNote(int selectedIndex) {
+        byte[] sampleNotes = {DO, MI, SO, HIGH_DO};
+        int randomIndex = (int) (Math.random() * 4);
+        
+        notes[selectedIndex] = sampleNotes[randomIndex];
+    }
+    
+    public static void listenSingleNote() {
+    	byte[] ledBit = {0, 0, 0, 0, 0, 0, 0, 0};
+    	ledBit[getConvertedLEDValueFromNote(notes[listenIndex])] = 1;
+    	ledBit[getConvertedLEDValueFromNote(notes[listenIndex]) + 1] = 1;
+    	driver.displayLED(ledBit);
+    	changeArrowBoard(notes[listenIndex]);
+    	
+    	delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		driver.playNote(notes[listenIndex]);
+        	}
+        }, 100);
+    	
+    	delayHandler.postDelayed(new Runnable() {
+        	public void run() {
+        		changeArrowBoard(-1);
+        		
+        		if (listenIndex == round) {
+        			driver.displayLED(EMPTY_BIT);
+        			playPhase();
+        		} else {
+        			listenIndex += 1;
+        			listenSingleNote();
+        		}
+        	}
+        }, 600);
+    }
+    
+    public static void judgeSingleNote(int currentGuess) {
+        if (guessIndex > round) return;
+        
+        if (currentGuess == notes[guessIndex]) {
+            score += round;
+            guessIndex += 1;
+            
+            byte[] ledBit = {0, 0, 0, 0, 0, 0, 0, 0};
+            ledBit[getConvertedLEDValueFromNote(currentGuess)] = 1;
+            ledBit[getConvertedLEDValueFromNote(currentGuess) + 1] = 1;
+            
+            changeArrowBoard(currentGuess);
+            driver.displayLED(ledBit);
+            
+            delayHandler.postDelayed(new Runnable() {
+            	public void run() {
+            		driver.playNote(notes[guessIndex - 1]);
+            	}
+            }, 100);
+            
+            delayHandler.postDelayed(new Runnable() {
+            	public void run() {
+            		changeArrowBoard(-1);
+            		driver.displayLED(EMPTY_BIT);
+            	}
+            }, 600);
+            
+            if (guessIndex > round) {
+                correctAnswerPhase();
+            }
+        } else {
+            wrongAnswerPhase();
+        }
+    }
+    
+    public static int getConvertedLEDValueFromNote(int noteValue) {
+        switch (noteValue) {
+            case DO:
+            	return 6;
+            case MI:
+            	return 4;
+            case SO:
+                return 2;
+            case HIGH_DO:
+                return 0;
+            default:
+                return 0;
+        }
+    }
+    
+    public static void changeArrowBoard(int arrowCode) {
+        switch (arrowCode) {
+            case DO:
+                arrowBoard.setImageResource(R.drawable.board_bottom);
+                break;
+            case MI:
+                arrowBoard.setImageResource(R.drawable.board_left);
+                break;
+            case SO:
+                arrowBoard.setImageResource(R.drawable.board_right);
+                break;
+            case HIGH_DO:
+                arrowBoard.setImageResource(R.drawable.board_top);
+                break;
+            default:
+                arrowBoard.setImageResource(R.drawable.board_disabled);
+        }
+    }
+    
+    public class ScoreThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            
+            while (true) {
+            	if (isScoreThreadRunning) {
+            		byte[] data = {0, 0, 0, 0, 0, 0};
+                
+            		data[0] = (byte) (score / 100000 % 10);
+            		data[1] = (byte) (score / 10000 % 10);
+            		data[2] = (byte) (score / 1000 % 10);
+            		data[3] = (byte) (score / 100 % 10);
+            		data[4] = (byte) (score / 10 % 10);
+            		data[5] = (byte) (score % 10);
+                
+            		driver.displaySegment(data);
+            	}
+            }
+        }
+    }
+    
+    
+    public Handler pushButtonHandler = new Handler() {
+        public void handleMessage(Message message) {
+            switch (message.arg1) {    
+                case CENTER:
+                    if (buttonLocker == NOT_STARTED) {
+                        startPhase();
+                    }
+                    break;
+                case DO:
+                case MI:
+                case SO:
+                case HIGH_DO:
+                    if (buttonLocker == OPEN) {
+                        judgeSingleNote(message.arg1);
+                    }
+                    break;
+            }
+        }
+    };
+    
+    @Override
+    public void onReceive(int value) {
+        Message text = Message.obtain();
+        text.arg1 = value;
+        pushButtonHandler.sendMessage(text);
+    }
+}
